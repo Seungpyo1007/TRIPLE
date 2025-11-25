9f0ee082d684ca42f0e213fd13cdc70eb9ee7a29
Seungpyo Hong <rush94434@gmail.com>
2025-11-25 15:02:41 +0900

    feat: 기본 세팅 & ViewController -> MainViewController 이름 바꾸기 & MainViewController와 SideMenuViewController 기본 세팅

--- TRIPLE/AppDelegate.swift
+++ TRIPLE/AppDelegate.swift
@@ -9,28 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // A를 rootViewController로 설정
+        let aVC = MainViewController(nibName: "MainViewController", bundle: nil)
+        // 윈도우 생성
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // 윈도우 rootViewController 설정
+        window?.rootViewController = aVC
+        // 윈도우 표시
+        window?.makeKeyAndVisible()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
 }
-
---
+++ TRIPLE/ViewControllers/MainViewController.swift
@@ -0,0 +1,102 @@
+//
+//  MainViewController.swift
+//  TRIPLE
+//
+//  Created by 홍승표 on 11/24/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    // 사이드바 컨트롤러
+    private var sideMenuViewController: SideMenuViewController!
+    //사이드바 그림자 영역
+    private var sideMenuShadowView: UIView!
+    //사이드바 가로 넓이 + 회전시 폭
+    private var sideMenuRevealWidth: CGFloat = 260
+    private let paddingForRotation: CGFloat = 150
+    //열려있는가
+    private var isExpanded: Bool = false
+    // 사이드메뉴 열기/닫기 설정용 LC
+    private var sideMenuTrailingConstraint: NSLayoutConstraint!
+   
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        // 사이드바 추가
+        self.setBasicSideMenu()
+    }
+
+    // 수동으로 버튼 누르면 만든 뷰 열기
+    @IBAction func openSlideMenuPro(_ sender: Any) {
+        self.sideMenuState(expanded: self.isExpanded ? false : true)
+    }
+    
+    
+    //MARK: - 수동제작 사이드 메뉴 관련 코드들
+    func setBasicSideMenu(){
+        // Shadow Background View
+        self.sideMenuShadowView = UIView(frame: self.view.bounds)
+        self.sideMenuShadowView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.sideMenuShadowView.backgroundColor = .black
+        self.sideMenuShadowView.alpha = 0.0
+        // 그림자 영역 클릭시 창 닫기
+        let closeShadowClick = UITapGestureRecognizer(target: self, action: #selector(sideMenuState(tapRecog:)))
+        self.sideMenuShadowView.addGestureRecognizer(closeShadowClick)
+        // 그림자 영역 추가
+        view.addSubview(self.sideMenuShadowView)
+
+        // Side Menu - 스토리보드 없이 XIB 또는 코드로 인스턴스화
+        if Bundle.main.path(forResource: "SideMenuViewController", ofType: "nib") != nil ||
+            Bundle.main.path(forResource: "SideMenuViewController", ofType: "xib") != nil {
+            self.sideMenuViewController = SideMenuViewController(nibName: "SideMenuViewController", bundle: .main)
+        } else {
+            self.sideMenuViewController = SideMenuViewController()
+        }
+   
+        // 사이드바 영역 추가
+        view.addSubview(self.sideMenuViewController!.view)
+        addChild(self.sideMenuViewController!) // 뷰가 추가될 영역 설정
+        self.sideMenuViewController!.didMove(toParent: self)
+
+        // 사이드 메뉴 레이아웃 잡기
+        self.sideMenuViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        self.sideMenuTrailingConstraint = self.sideMenuViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -self.sideMenuRevealWidth - self.paddingForRotation)
+        self.sideMenuTrailingConstraint.isActive = true
+    
+        NSLayoutConstraint.activate([
+            self.sideMenuViewController.view.widthAnchor.constraint(equalToConstant: self.sideMenuRevealWidth),
+            self.sideMenuViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            self.sideMenuViewController.view.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
+    // 그림자 부분 클릭시 닫기 연결용
+    @objc func sideMenuState(tapRecog: UITapGestureRecognizer){
+        // 열려있으면 닫기
+        self.sideMenuState(expanded: self.isExpanded ? false : true)
+    }
+    // 액션 설정 부분
+    func sideMenuState(expanded: Bool) {
+        if expanded {
+            self.animateSideMenu(targetPosition:  0 ) { _ in
+                self.isExpanded = true
+            }
+            // Animate Shadow (Fade In)
+            UIView.animate(withDuration: 0.5) { self.sideMenuShadowView.alpha = 0.6 }
+        }
+        else {
+            self.animateSideMenu(targetPosition: (-self.sideMenuRevealWidth - self.paddingForRotation)) { _ in
+                self.isExpanded = false
+            }
+            // Animate Shadow (Fade Out)
+            UIView.animate(withDuration: 0.5) { self.sideMenuShadowView.alpha = 0.0 }
+        }
+    }
+    
+    func animateSideMenu(targetPosition: CGFloat, completion: @escaping (Bool) -> ()) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: .layoutSubviews, animations: {
+            self.sideMenuTrailingConstraint.constant = targetPosition
+            self.view.layoutIfNeeded()
+        }, completion: completion)
+    }
+}
+
