2ff803ecd09a0980cfdde98aabeff5c43c3defc1
Seungpyo Hong <rush94434@gmail.com>
2025-12-12 17:49:39 +0900

    feat: GoogleMaps APIKEY 테스트로 AppDelegate에 추가

--- TRIPLE/AppDelegate.swift
+++ TRIPLE/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Load Google Maps API Key from Secret.plist
+        if let path = Bundle.main.path(forResource: "Secret", ofType: "plist"),
+           let dict = NSDictionary(contentsOfFile: path) as? [String: Any],
+           let apiKey = dict["GoogleMaps-API-KEY"] as? String, !apiKey.isEmpty {
+            GMSServices.provideAPIKey(apiKey)
+        } else {
+            assertionFailure("Missing or empty APIKEY in Secret.plist")
+        }
         let window = UIWindow(frame: UIScreen.main.bounds)
         let navController = UINavigationController()
         navController.setNavigationBarHidden(true, animated: false) // Default NavigationBar 숨기기
