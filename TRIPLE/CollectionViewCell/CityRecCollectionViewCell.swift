73e8e10b44a77106b8a5af86fdbdee552b5523e2
Seungpyo Hong <rush94434@gmail.com>
2025-12-15 17:50:56 +0900

    feat: CityRecCollectionViewCell imageView Outlet 연결 완료 및 ViewModel 데이터랑 연결 완료

--- TRIPLE/CollectionViewCell/CityRecCollectionViewCell.swift
+++ TRIPLE/CollectionViewCell/CityRecCollectionViewCell.swift
@@ -11,19 +11,39 @@ class CityRecCollectionViewCell: UICollectionViewCell {
 
     static let reuseIdentifier = "CityRecCollectionViewCell"
 
+    @IBOutlet weak var imageView: UIImageView!
+    private var loadToken: UUID?
+
     override func awakeFromNib() {
         super.awakeFromNib()
         contentView.backgroundColor = .secondarySystemBackground
         contentView.layer.cornerRadius = 12
         contentView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.image = nil
+        loadToken = nil
         // TODO: Reset XIB outlets when added later
     }
 
     func configure(with placeholder: String) {
-        // TODO: Bind XIB outlets with real model when available
+        // This configure method now only handles text or placeholder binding
+    }
+    
+    func configureImage(viewModel: CityRecCollectionViewModel, indexPath: IndexPath, collectionView: UICollectionView) {
+        let targetSize = CGSize(width: max(1, Int(bounds.width)), height: max(1, Int(bounds.height)))
+        let token = UUID()
+        self.loadToken = token
+        self.imageView.image = nil
+        viewModel.loadPhotoForItem(at: indexPath.item, targetSize: targetSize) { [weak self] image in
+            DispatchQueue.main.async {
+                guard let self = self, self.loadToken == token else { return }
+                self.imageView.image = image
+            }
+        }
     }
 }
