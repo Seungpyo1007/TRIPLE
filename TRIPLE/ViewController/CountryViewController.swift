f09b7b5a83cb64f7c08a9b9360d52564dedb9d69
Seungpyo Hong <rush94434@gmail.com>
2025-12-17 17:49:15 +0900

    feat: CountryViewController IBOutlet 추가 및 Cell 여러가지 View,Button 추가, 이름 수정 그리고 제약 조건 걸기 완료 (닫는 버튼은 구현 완료)

--- TRIPLE/ViewController/CountryViewController.swift
+++ TRIPLE/ViewController/CountryViewController.swift
@@ -9,13 +9,25 @@ import UIKit
 import RiveRuntime
 
 class CountryViewController: UIViewController {
-    @IBOutlet weak var mainView: RiveView!
-    var sampleVM = RiveViewModel(fileName: "Snow")
+    @IBOutlet weak var riveView: RiveView!
     
+    @IBOutlet weak var countryKorLabel: UILabel!
+    @IBOutlet weak var countryEngLabel: UILabel!
+    @IBOutlet weak var countryTownLabel: UILabel!
+    @IBOutlet weak var countryFirstCityLabel: UILabel!
+    @IBOutlet weak var countrySecondCityLabel: UILabel!
+    
+    var sampleVM = RiveViewModel(fileName: "Snow")
+        
+    @IBAction func closeButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func openSideMenu(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        sampleVM.setView(mainView)
-
+        sampleVM.setView(riveView)
+        
     }
     
 }
